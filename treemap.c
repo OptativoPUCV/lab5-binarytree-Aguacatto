b79f1d6ef03b1c1774e3f0bf6ea24eb85f9f185a
Your Name <you@example.com>
2024-05-09 05:10:32 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -100,11 +100,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
 
 TreeNode * minimum(TreeNode * x)
 {
-    while(x->left != NULL)
-    {
-        x = x->left;
-    }
-    return x;
+    return NULL;
 }
 
 
